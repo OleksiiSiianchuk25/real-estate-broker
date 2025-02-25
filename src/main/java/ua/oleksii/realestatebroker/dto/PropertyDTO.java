5966b636dd71508d2f81d19e3cdf44b8ff6f0e2a
Oleksii Siianchuk <oleksii.siianchuk@gmail.com>
2025-02-25 20:13:05 +0200

    Added updating and creating of property

---
+++ src/main/java/ua/oleksii/realestatebroker/dto/PropertyDTO.java
@@ -0,0 +1,107 @@
+package ua.oleksii.realestatebroker.dto;
+
+import ua.oleksii.realestatebroker.model.Property.Status;
+import ua.oleksii.realestatebroker.model.Property.Type;
+import java.math.BigDecimal;
+
+public class PropertyDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private BigDecimal price;
+    private Type type;
+    private Status status;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private String city;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
